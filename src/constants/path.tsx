27bcad6d39ac8a2e6006d05e2d5427677b90bb07
t100ta <cjx.open_d@me.com>
2025-01-10 19:21:55 +0900

    CLIENT SECRETを削除

--- src/constants/path.tsx
+++ src/constants/path.tsx
@@ -1,5 +1,4 @@
 export const APP_URI = import.meta.env.VITE_APP_URI;
 export const AUTH_API_URI = import.meta.env.VITE_AUTH_API_URI;
 export const TWITCH_CLIENT_ID = import.meta.env.VITE_TWITCH_CLIENT_ID;
-export const TWITCH_CLIENT_SECRET = import.meta.env.VITE_TWITCH_CLIENT_SECRET;
 export const TWITCH_API_URI = "https://api.twitch.tv/helix";
