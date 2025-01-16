637bb3cf568797f1a7cdda461bba0423da9b1ffe
t100ta <cjx.open_d@me.com>
2025-01-17 01:49:53 +0900

    useMutateValidationのエラーハンドリングを追加

--- src/hooks/useMutateValidation.ts
+++ src/hooks/useMutateValidation.ts
@@ -4,22 +4,36 @@ import axios, { HttpStatusCode } from "axios";
 export const useMutateValidation = () => {
   const executeValidation = useMutation({
     mutationFn: async (oauthToken: string) => {
-      const { data, status } = await axios.get(
-        `https://id.twitch.tv/oauth2/validate`,
-        {
-          headers: {
-            Authorization: `OAuth ${oauthToken}`,
-          },
-        }
-      );
-
-      if (status != HttpStatusCode.Ok) {
-        console.error(
-          `Token is invalid. /oauth2/validate returned status code ${data.status}`
+      try {
+        const { data, status } = await axios.get(
+          `https://id.twitch.tv/oauth2/validate`,
+          {
+            headers: {
+              Authorization: `OAuth ${oauthToken}`,
+            },
+          }
         );
-        console.error(data.data);
+
+        if (status != HttpStatusCode.Ok) {
+          console.error(
+            `Token is invalid. /oauth2/validate returned status code ${data.status}`
+          );
+          console.error(data.data);
+          throw new Error("Token is invalid");
+        }
+        return data;
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          console.error(
+            "Validation error:",
+            error.response?.data || error.message
+          );
+          throw new Error("Failed to validate");
+        } else {
+          console.error("An unexpected error occurred:", error);
+          throw new Error("An unexpected error occurred");
+        }
       }
-      return data;
     },
   });
   return executeValidation;
